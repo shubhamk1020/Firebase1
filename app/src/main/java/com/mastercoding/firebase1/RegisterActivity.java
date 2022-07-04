d76cfe03000458fdf138d90a1e2f208b2a977946
shubham1020k <gyaninfo128@gmail.com>
2022-07-04 09:01:05 +0530

    activity_login.xml AndroidManifest.xml LoginActivity MainActivity RegisterActivity

--- app/src/main/java/com/mastercoding/firebase1/RegisterActivity.java
+++ app/src/main/java/com/mastercoding/firebase1/RegisterActivity.java
@@ -1,14 +1,117 @@
 package com.mastercoding.firebase1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.sax.StartElementListener;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    //Widgets
+    EditText userET, passET, emailET;
+    Button signbtn;
+
+    // Firebase
+    FirebaseAuth auth;
+    DatabaseReference myRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // Initialize
+        userET = findViewById(R.id.editTextuser);
+        passET = findViewById(R.id.editTextPassword);
+        emailET = findViewById(R.id.editTextEmail);
+
+        signbtn = findViewById(R.id.signupbtn);
+
+        //Firebase Auth
+        // auth = FirebaseAuth.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        //Adding Event Listner to Button Register
+        signbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username_text = userET.getText().toString();
+                String email_text = emailET.getText().toString();
+                String pass_text = passET.getText().toString();
+
+                if(TextUtils.isEmpty(username_text) || TextUtils.isEmpty(email_text)
+                        || TextUtils.isEmpty(pass_text)){
+                Toast.makeText(RegisterActivity.this, "Please Fill All Fields", Toast.LENGTH_SHORT).show();
+            }
+            else{
+              RegisterNow(username_text,email_text,pass_text);
+            }
+
+            }
+        });
+
     }
+
+    private void RegisterNow(final String username, String email, String password){
+        auth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            String userid = firebaseUser.getUid();
+
+                            myRef = FirebaseDatabase.getInstance().getReference
+                                    ("MyUsers").child(userid);
+
+                            // HasMaps
+                            HashMap<String, String> hashMap = new HashMap<>();
+                            hashMap.put("id", userid);
+                            hashMap.put("username", username);
+                            hashMap.put("imageURL","default");
+
+                            // Opening the Main Activity after Successful
+
+                            myRef.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Intent i = new Intent(RegisterActivity.this,MainActivity.class);
+                                        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                                        startActivity(i);
+                                        finish();
+
+                                    }
+                                }
+                            });
+
+                        }
+                                else{
+                            Toast.makeText(RegisterActivity.this, "Invailed Email or Password", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
 }
